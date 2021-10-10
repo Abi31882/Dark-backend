a86f48d0dfecf29af2477612b9df4a73321c5ae1
Abi31882 <abhinavchaudhary348@gmail.com>
2021-10-10 14:19:57 +0530

    ini

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -4,6 +4,7 @@ const Category = require('../models/categoryModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 const AppError = require('../utils/appError');
+const APIFeatures = require('../utils/apiFeatures');
 
 const multerStorage = multer.memoryStorage();
 
@@ -53,7 +54,7 @@ exports.setMakerIds = (req, res, next) => {
   next();
 };
 
-exports.getAllCategories = factory.getAll(Category);
+// exports.getAllCategories = factory.getAll(Category);
 exports.getCategory = factory.getOne(Category, {
   path: 'maker',
   select: 'name role',
@@ -61,3 +62,27 @@ exports.getCategory = factory.getOne(Category, {
 exports.createCategory = factory.createOne(Category);
 exports.updateCategory = factory.updateOne(Category);
 exports.deleteCategory = factory.deleteOne(Category);
+
+exports.getAllCategories = catchAsync(async (req, res, next) => {
+  if (req.query.query) {
+    const docu = await Category.find({
+      $or: [{ categoryName: { $regex: req.query.query } }],
+    });
+
+    res.status(200).json(docu);
+  } else {
+    // to allow for nested GET reviews on product (hack)
+    let filter = {};
+    if (req.params.categoryId)
+      filter = { productCategory: req.params.categoryId };
+    const features = new APIFeatures(Category.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const doc = await features.query;
+
+    // SEND RESPONSE
+    res.status(200).json(doc);
+  }
+});
