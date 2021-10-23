2784cdfea1a9af9bfec8eec169f7fb9ccd5f6e15
Abi31882 <abhinavchaudhary348@gmail.com>
2021-10-23 07:56:24 +0530

    tried something

--- controllers/productController.js
+++ controllers/productController.js
@@ -39,7 +39,9 @@ exports.resizeProductImages = catchAsync(async (req, res, next) => {
     return next();
 
   // 1) Front image
-  req.body.imageFront = `product-${req.params.id}-${Date.now()}-front.jpeg`;
+  req.body.imageFront = `${req.protocol}://${req.get(
+    'host'
+  )}/products/product-${req.params.id}-${Date.now()}-front.jpeg`;
   await sharp(req.files.imageFront[0].buffer)
     .resize(150, 150)
     .toFormat('jpeg')
@@ -47,7 +49,9 @@ exports.resizeProductImages = catchAsync(async (req, res, next) => {
     .toFile(`public/img/products/${req.body.imageFront}`);
 
   // 2) cover image
-  req.body.imageCover = `product-${req.params.id}-${Date.now()}-cover.jpeg`;
+  req.body.imageCover = `${req.protocol}://${req.get(
+    'host'
+  )}/products/product-${req.params.id}-${Date.now()}-cover.jpeg`;
   await sharp(req.files.imageCover[0].buffer)
     .resize(640, 640)
     .toFormat('jpeg')
@@ -59,7 +63,9 @@ exports.resizeProductImages = catchAsync(async (req, res, next) => {
 
   await Promise.all(
     req.files.images.map(async (file, i) => {
-      const filename = `product-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+      const filename = `${req.protocol}://${req.get('host')}/products/product-${
+        req.params.id
+      }-${Date.now()}-${i + 1}.jpeg`;
 
       await sharp(file.buffer)
         .resize(640, 640)
